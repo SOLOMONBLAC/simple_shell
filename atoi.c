6a3543d76c7da38e231ed4535b2436b5dad4e2b9
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-07-26 13:37:31 -0700

    Refactor atoi.c: Improved function names, logic, and readability.

--- atoi.c
+++ atoi.c
@@ -1,74 +1,70 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * is_interactive_mode - checks if the shell is in interactive mode
+ * @info: pointer to the shell information struct
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * Return: 1 if in interactive mode, 0 otherwise
  */
-int interactive(info_t *info)
+
+int is_interactive_mode(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
+ * is_delimiter - checks if a character is a delimiter
+ * @c: the character to check
+ * @delim: the delimiter string
+ *
+ * Return: 1 if the character is a delimiter, 0 otherwise
  */
-int is_delim(char c, char *delim)
+int is_delimiter(char c, char *delim)
 {
-	while (*delim)
-		if (*delim++ == c)
+	while (*delim != '\0')
+	{
+		if (*delim == c)
 			return (1);
+		delim++;
+	}
 	return (0);
 }
 
 /**
- *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
+ * is_alphabetic - checks if a character is alphabetic
+ * @c: the character to check
+ *
+ * Return: 1 if the character is alphabetic, 0 otherwise
  */
-
-int _isalpha(int c)
+int is_alphabetic(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
 
 /**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Return: the converted integer, 0 if no numbers in the string
  */
-
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
+	int i, sign = 1, flag = 0;
+	unsigned int res = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
-	{
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+    {
 		if (s[i] == '-')
 			sign *= -1;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
+			res = res * 10 + (s[i] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
 	}
 
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
+	return (sign * res);
 }
