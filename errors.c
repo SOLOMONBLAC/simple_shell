c831da6d25a93a347b22c25eca0d72ad8186ac5b
SOLOMONBLAC <Nwajiobisolo@gmail.com>
2023-07-27 04:30:43 -0700

    error check

--- errors.c
+++ errors.c
@@ -1,77 +1,88 @@
-#ifndef SHELL_H
-#define SHELL_H
 
-#include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
+#include "shell.h"
 
-#define BUF_FLUSH '\0'
-#define WRITE_BUF_SIZE 1024
-
-void _eputs(char *str);
-int _eputchar(char c);
-int _putfd(char c, int fd);
-int _putsfd(char *str, int fd);
-
+/**
+ * _eputs - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
 void _eputs(char *str)
 {
-    int i = 0;
-    char c;
+	int i = 0;
 
-    if (!str)
-        return;
-
-    while ((c = str[i]) != '\0')
-    {
-        _eputchar(c);
-        i++;
-    }
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
 }
 
+/**
+ * _eputchar - writes the character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _eputchar(char c)
 {
-    static int i = 0;
-    static char buf[WRITE_BUF_SIZE];
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
 
-    if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-    {
-        write(2, buf, i);
-        i = 0;
-    }
-    if (c != BUF_FLUSH)
-        buf[i++] = c;
-    return (1);
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
 
+/**
+ * _putfd - writes the character c to given fd
+ * @c: The character to print
+ * @fd: The filedescriptor to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putfd(char c, int fd)
 {
-    static int i = 0;
-    static char buf[WRITE_BUF_SIZE];
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
 
-    if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-    {
-        write(fd, buf, i);
-        i = 0;
-    }
-    if (c != BUF_FLUSH)
-        buf[i++] = c;
-    return (1);
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
 
+/**
+ * _putsfd - prints an input string
+ * @str: the string to be printed
+ * @fd: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
 int _putsfd(char *str, int fd)
 {
-    int i = 0;
-    char c;
+	int i = 0;
 
-    if (!str)
-        return (0);
-
-    while ((c = *str++) != '\0')
-    {
-        i += _putfd(c, fd);
-    }
-    return (i);
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
 }
 
-#endif /* SHELL_H */
 
